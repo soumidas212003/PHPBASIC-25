48564eb6f3e504374533c3d34d4236020b37d909
Soumita Das <soumitad991@gmail.com>
2025-07-05 19:48:14 +0530

    Echo use

---
+++ 2echo.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Soumita Das<br>";
+    echo "Soumita"," Das<br>"; //multiple values
+    echo "Soumita"." Das"; //concatenate
+    echo "<h1><i>Soumita"." Das</i></h1>"; //concatenate
+    echo 2358 ."<br>"; //Numeric value 
+    echo 23.58; //Decimal value
+    //"echo" can print multiple values but "print" can't print multiple values
+    //echo is faster than print
+?>
