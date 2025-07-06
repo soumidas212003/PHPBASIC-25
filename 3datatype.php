d1b3ca549756ed2fa8618ca5d5814e7386f36530
Soumita Das <soumitad991@gmail.com>
2025-07-06 15:49:23 +0530

    Echo use

---
+++ 3datatype.php
@@ -0,0 +1,16 @@
+<?php
+    //$x="Soumita Das"; 
+    //$x=2500;
+    //$x="2500";
+    //$x=2500.50;
+    //$x="2500.50";
+    //$x=true;
+    //$x=false;
+    $x=null;
+    echo $x . "<br>";   
+    var_dump($x);//show variable value & variable datatype
+    echo("<br>");
+    $y=array("html","css","js");
+    echo $y[1] . "<br>";   
+    var_dump($y);
+?>
