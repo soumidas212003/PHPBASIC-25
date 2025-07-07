4cabe37f08ce37462335c1c25dc750d7f8f61e67
Soumita Das <soumitad991@gmail.com>
2025-07-07 23:54:53 +0530

    Arithmetic Operator

---
+++ 5arithmeticoperator.php
@@ -0,0 +1,24 @@
+<?php
+//Arithmetic Operator
+
+//$a=10.55;
+$a = 10;
+$b = 3;
+//$c = $a + $b;
+//$c = $a - $b;
+//$c = $a * $b;
+//$c = $a / $b;
+//$c = $a % $b;
+$c = $a ** $b;
+echo $c . "<br>";
+$d=1;
+$d++;
+echo $d . "<br>";
+$e=2;
+$e--;
+echo $e . "<br>";
+$f = $a + $b * 2;
+echo $f . "<br>";
+$g = ($a + $b) * 2;
+echo $g;
+?>
