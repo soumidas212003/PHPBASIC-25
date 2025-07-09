cd98d518be45b3e5c9ff86e40fe2b5d159a74be5
Soumita Das <soumitad991@gmail.com>
2025-07-09 21:12:47 +0530

    Operator

---
+++ 6assignmentoperator.php
@@ -0,0 +1,12 @@
+<?php
+// Assignment Operator
+$a = 10;
+$b = 3;
+//$a += $b;
+//$a -= $b;
+//$a *= $b;
+//$a /= $b;
+//$a %= $b;
+$a **= $b;
+echo $a;
+?>
---
+++ 7comparisonoperator.php
@@ -0,0 +1,15 @@
+<?php
+//Compatison Operators
+$a=55;
+$b=75;
+//echo $a == $b; //Equal to
+//echo $a === $b; //Equal value and equal type
+//echo $a != $b; //Not equal 
+//echo $a <> $b; //Not equal 
+echo $a !== $b; //Not equal value and not equal type
+//echo $a > $b; //Greater than
+//echo $a < $b; //Less than
+//echo $a >= $b; //Greater than or equal to
+//echo $a <= $b; //Less than or equal to
+//echo $a <=> $b; //Spaceshop operator (It returns -1, 0 or 1 respectivelt less than, equal t, or greater than)
+?>
