23f8132aba70ab1175df9c7c8a6c05853b670e75
Soumita Das <soumitad991@gmail.com>
2025-07-11 19:55:05 +0530

    goto statement

---
+++ 18gotostatement.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello Soumita<br>";
+echo "Hello Soumita<br>";
+goto abc;
+echo "Hello Soumita<br>";
+echo "Hello Soumita<br>";
+echo "Hello Soumita<br>";
+echo "Hello Soumita<br>";
+abc:
+echo "Hey this is label ABC..";
+?>
